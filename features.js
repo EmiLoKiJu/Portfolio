e090ecfa9f5bc0bdc644dae0ed4a0ca73fe36583
unknown <gabrielrozas1996@gmail.com>
2023-04-11 09:08:35 -0800

    Add x image

--- features.js
+++ features.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-plusplus */
 const button = document.querySelector('.menu');
 const element = document.querySelector('.menuopen');
-const links = document.querySelectorAll('.menuopen a');
+const links = document.querySelectorAll('.menuopen a, .menuopen img');
 
 button.addEventListener('click', () => {
-  element.style.display = 'block';
+  element.style.display = 'flex';
 });
 
 for (let i = 0; i < links.length; i++) {
