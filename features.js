e04b7509b8d472a403363c27fae5112f70925ed0
unknown <gabrielrozas1996@gmail.com>
2023-04-10 18:29:21 -0800

    Fix linters

--- features.js
+++ features.js
@@ -2,12 +2,12 @@ let button = document.querySelector(".menu");
 let element = document.querySelector(".menuopen");
 let links = document.querySelectorAll(".menuopen a");
 
-button.addEventListener("click", () {
+button.addEventListener("click", () => {
   element.style.display = "block";
 });
 
 for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", () {
+  links[i].addEventListener("click", () => {
     element.style.display = "none";
   });
 }
