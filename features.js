08cc1f5a9fc5628d854e61e175800fec192ec68d
unknown <gabrielrozas1996@gmail.com>
2023-04-10 18:36:09 -0800

    Fix linters

--- features.js
+++ features.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-plusplus */
-const button = document.querySelector(".menu");
-const element = document.querySelector(".menuopen");
-const links = document.querySelectorAll(".menuopen a");
+const button = document.querySelector('.menu');
+const element = document.querySelector('.menuopen');
+const links = document.querySelectorAll('.menuopen a');
 
-button.addEventListener("click", () => {
-  element.style.display = "block";
+button.addEventListener('click', () => {
+  element.style.display = 'block';
 });
 
 for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", () => {
-    element.style.display = "none";
+  links[i].addEventListener('click', () => {
+    element.style.display = 'none';
   });
 }
