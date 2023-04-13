a66cb97119f6f79225586affd44f1a1088cd560f
unknown <gabrielrozas1996@gmail.com>
2023-04-12 17:34:39 -0800

    Fix some margins, add some buttons

--- features.js
+++ features.js
@@ -1,13 +1,17 @@
-const { createElement } = require("parse5/lib/tree-adapters/default");
-
 /* eslint-disable no-plusplus */
+const mainpdetailbutton = document.querySelector('.seeproject1');
+const p1pdetailbutton = document.querySelector('.bp1');
+const p2pdetailbutton = document.querySelector('.bp2');
+const p3pdetailbutton = document.querySelector('.bp3');
+const p4pdetailbutton = document.querySelector('.bp4');
+const p5pdetailbutton = document.querySelector('.bp5');
+const p6pdetailbutton = document.querySelector('.bp6');
 const button = document.querySelector('.menu');
 const element = document.querySelector('.menuopen');
 const links = document.querySelectorAll('.menuopen a, .menuopen img');
-const mainpdetailbutton = document.querySelector('.seeproject1');
-const p1p4pdetailbutton = document.querySelector('.p1 seeproject2', '.p4 seeproject2');
-const p2p5pdetailbutton = document.querySelector('.p2 seeproject2', '.p5 seeproject2');
-const p3p6pdetailbutton = document.querySelector('.p3 seeproject2', '.p6 seeproject2');
+let buttonarr = [mainpdetailbutton, p1pdetailbutton, p2pdetailbutton, p3pdetailbutton, p4pdetailbutton, p5pdetailbutton, p6pdetailbutton];
+var closeprojectdetails = undefined;
+
 const projects = {
   'Multi-Post stories': {
     features: ['HTML', 'CSS', 'Bootstrap', 'Ruby on rails'],
@@ -60,14 +64,50 @@ const projects = {
     ligula, porta ac lorem vitae, ultrices cursus feliz",
     seelive: '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">',
     seesrc: '<a href="https://github.com/EmiLoKiJu/Portfolio">'
+  },
+  'Profesional Art Printing Data More': {
+    features: ['HTML', 'Bootstrap', 'Ruby on rails'],
+    imagesrc: 'src="./img/projectdetailimg.png"',
+    projectdescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Suspendisse interdum a dolor sit amet egestas. Vivamus lacus dui, dignissim a odio \
+    lobortis, ornare cursus mauris. Phasellus mollis mollis ornare. Nullam ornare, \
+    libero non ornare sodales, leo neque dapibus ligula, eget maximus enim eros vitae eros. \
+    Quisque leo erat, luctus at leo sit amet, consectetur elementum odio. Proin vulputate \
+    erat et felis bibendum sollicitudin. Nulla pretium lobortis ligula eu feugiat. Sed nibh \
+    ligula, porta ac lorem vitae, ultrices cursus feliz",
+    seelive: '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">',
+    seesrc: '<a href="https://github.com/EmiLoKiJu/Portfolio">'
+  },
+  'Data Dashboard Healthcare': {
+    features: ['HTML', 'CSS', 'Ruby on rails'],
+    imagesrc: 'src="./img/projectdetailimg.png"',
+    projectdescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Suspendisse interdum a dolor sit amet egestas. Vivamus lacus dui, dignissim a odio \
+    lobortis, ornare cursus mauris. Phasellus mollis mollis ornare. Nullam ornare, \
+    libero non ornare sodales, leo neque dapibus ligula, eget maximus enim eros vitae eros. \
+    Quisque leo erat, luctus at leo sit amet, consectetur elementum odio. Proin vulputate \
+    erat et felis bibendum sollicitudin. Nulla pretium lobortis ligula eu feugiat. Sed nibh \
+    ligula, porta ac lorem vitae, ultrices cursus feliz",
+    seelive: '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">',
+    seesrc: '<a href="https://github.com/EmiLoKiJu/Portfolio">'
+  },
+  'Website Portfolio ': {
+    features: ['HTML', 'Bootstrap', 'Ruby on rails'],
+    imagesrc: 'src="./img/projectdetailimg.png"',
+    projectdescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Suspendisse interdum a dolor sit amet egestas. Vivamus lacus dui, dignissim a odio \
+    lobortis, ornare cursus mauris. Phasellus mollis mollis ornare. Nullam ornare, \
+    libero non ornare sodales, leo neque dapibus ligula, eget maximus enim eros vitae eros. \
+    Quisque leo erat, luctus at leo sit amet, consectetur elementum odio. Proin vulputate \
+    erat et felis bibendum sollicitudin. Nulla pretium lobortis ligula eu feugiat. Sed nibh \
+    ligula, porta ac lorem vitae, ultrices cursus feliz",
+    seelive: '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">',
+    seesrc: '<a href="https://github.com/EmiLoKiJu/Portfolio">'
   }
 }
+
 const mainp = document.querySelector('body');
 
-
-//document.body.appendChild(div1);
-//let div2 = document.querySelector('pdetailswin');
-//div1.parentNode.removeChild(div1);
 function createpwin(proj) {
   let div1 = document.createElement('div');
   div1.classList.add('pdetailswin');
@@ -86,7 +126,7 @@ function createpwin(proj) {
     <div class="dflex spacebetween">\
       <h2>'+Object.keys(projects)[proj]+'</h2>\
       <div class="xbuttonp">\
-        '+closeprojectdetails+'\
+        <img class="ximg" src="./img/Icon.png" alt="X">\
       </div>\
     </div>\
     <div class="imageanddesc">\
@@ -106,23 +146,20 @@ function createpwin(proj) {
     </div>\
   </div>';
   mainp.appendChild(div1);
-  console.log(mainpdetailbutton);
+  closeprojectdetails = document.querySelector('.xbuttonp .ximg');
+  console.log(closeprojectdetails);
+  closeprojectdetails.addEventListener('click', () => {
+    div1.parentNode.removeChild(div1);
+    console.log("Removed child?");
+  });
 }
 
-mainpdetailbutton.addEventListener('click', () => {
-  createpwin(0);
-});
-
-let btn = document.createElement('button');
-//closeprojectdetails.addEventListener('click', () => {
-//  div1.parentNode.removeChild(div1);
-//});
-
-//p1p4pdetailbutton 
-//p2p5pdetailbutton 
-//p3p6pdetailbutton
-if (window.innerWidth >= 1024) {
-  
+for(let i=0;i<buttonarr.length;i++)
+{
+  buttonarr[i].addEventListener('click', () => {
+    createpwin(i);
+    console.log("i = ",i," , buttonarr[i] = ", buttonarr[i]);
+  });
 }
 
 button.addEventListener('click', () => {
